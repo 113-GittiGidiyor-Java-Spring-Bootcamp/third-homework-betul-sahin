b02e9032aabd6eb1e7c3a0f9f29c19ac2cdcc007
Betül Şahin <betulsahin3387@hotmail.com>
2021-08-23 12:18:58 +0300

    spring boot project has been initialized

---
+++ SchoolManagementSystemDemoV3/src/main/java/com/example/schoolmanagementsystemdemov3/SchoolManagementSystemDemoV3Application.java
@@ -0,0 +1,13 @@
+package com.example.schoolmanagementsystemdemov3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolManagementSystemDemoV3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolManagementSystemDemoV3Application.class, args);
+    }
+
+}
---
+++ SchoolManagementSystemDemoV3/src/test/java/com/example/schoolmanagementsystemdemov3/SchoolManagementSystemDemoV3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.schoolmanagementsystemdemov3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolManagementSystemDemoV3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
