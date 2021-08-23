741b9de61caf5335dd22d7e8fb238f89a0408837
Betül Şahin <betulsahin3387@hotmail.com>
2021-08-23 18:29:21 +0300

    some changes

--- SchoolManagementSystemDemoV3/src/main/java/com/betulsahin/schoolmanagementsystemdemov3/repository/InstructorRepository.java
+++ SchoolManagementSystemDemoV3/src/main/java/com/betulsahin/schoolmanagementsystemdemov3/repository/InstructorRepository.java
@@ -1,6 +1,7 @@
 package com.betulsahin.schoolmanagementsystemdemov3.repository;
 
 import com.betulsahin.schoolmanagementsystemdemov3.entity.Instructor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,6 +10,10 @@ import java.util.List;
 @Repository
 public interface InstructorRepository extends CrudRepository<Instructor, Long> {
     List<Instructor> findAllByNameContainingIgnoreCase(String searchWord);
+
+    // TODO
+   /* @Query("SELECT max(p.salary) FROM Instructor i INNER JOIN permanentInstructor p WHERE p.id = i.id")
+    List<Instructor> findAllTop3BySalaryGreaterThan();*/
 
     void deleteByName(String name);
 }
