fd44ab60973e4107d6af0d5dc680e0543bd5caa0
Betül Şahin <betulsahin3387@hotmail.com>
2021-08-28 19:58:52 +0300

    added swagger config

--- SchoolManagementSystemDemoV3/src/main/java/com/betulsahin/schoolmanagementsystemdemov3/SchoolManagementSystemDemoV3Application.java
+++ SchoolManagementSystemDemoV3/src/main/java/com/betulsahin/schoolmanagementsystemdemov3/SchoolManagementSystemDemoV3Application.java
@@ -5,8 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class SchoolManagementSystemDemoV3Application {
 
     public static void main(String[] args) {
