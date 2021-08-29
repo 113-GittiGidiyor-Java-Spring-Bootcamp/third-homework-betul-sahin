0f12541b6d7776c39670771be50c243cb6ef93a6
Betül Şahin <betulsahin3387@hotmail.com>
2021-08-29 19:14:39 +0300

    changes

--- SchoolManagementSystemDemoV3/src/main/java/com/betulsahin/schoolmanagementsystemdemov3/repository/InstructorRepository.java
+++ SchoolManagementSystemDemoV3/src/main/java/com/betulsahin/schoolmanagementsystemdemov3/repository/InstructorRepository.java
@@ -13,9 +13,9 @@ public interface InstructorRepository extends CrudRepository<Instructor, Long> {
     List<Instructor> findAllByNameContainingIgnoreCase(String searchWord);
 
     // Top 3 salaries for PermanentInstructor
-    // LIMIT 3 calismadigi icin native query yaptim
+    // LIMIT 3 calismadigi icin native query e cevirdim.
     @Query(nativeQuery = true,
-           value="SELECT perm.salary FROM Instructor ins inner join PermanentInstructor perm on ins.id=perm.id ORDER BY 1 DESC LIMIT 3")
+           value="SELECT ins.name, perm.salary FROM Instructor ins inner join PermanentInstructor perm on ins.id=perm.id ORDER BY 2 DESC LIMIT 3")
     List<?> findAllTop3BySalaryGreaterThan();
 
     void deleteByName(String name);
